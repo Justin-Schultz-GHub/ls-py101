4ba7ede33bfe54f3c43753e3f76a9bc884f6fb22
Justin Schultz <SchultzJustin@ymail.com>
2024-03-28 22:14:52 +0000

    Refactored calculator and added NaN checks

--- lesson_2/mortgage_calculator.py
+++ lesson_2/mortgage_calculator.py
@@ -6,73 +6,110 @@ for each of them. I thought about refactoring the program to use one function
 for all 3 checks, but figured it was better to leave it like this in case I
 wanted to change the check logic for one value and not all.
 """
+import os
 
 def prompt(message):
     print(f'==> {message}')
 
 def invalid_loan(amount):
-    try:
-        float(amount)
-    except ValueError:
-        return True
+    if float('-inf') < float(amount) < float('inf'):
+        try:
+            float(amount)
+        except ValueError:
+            return True
 
-    return bool(float(amount) <= 0)
+        return float(amount) <= 0
+
+    else:
+        return True
 
 def invalid_int(pct):
-    try:
-        float(pct)
-    except ValueError:
-        return True
+    if float('-inf') < float(pct) < float('inf'):
+        try:
+            float(pct)
+        except ValueError:
+            return True
 
-    return bool(float(pct) <= 0)
+        return float(pct) < 0
+
+    else:
+        return True
 
 def invalid_dur(duration):
-    try:
-        float(duration)
-    except ValueError:
+    if float('-inf') < float(duration) < float('inf'):
+        try:
+            float(duration)
+        except ValueError:
+            return True
+
+        return float(duration) <= 0
+
+    else:
         return True
 
-    return bool(float(duration) <= 0)
-
-while True:
+def get_loan_amount():
     prompt("Please enter your loan amount. (e.g. $250,000 = 250000)")
-    ln_amnt = input()
+    amount = input()
 
-    while invalid_loan(ln_amnt):
-        prompt("Please enter a valid amount.")
-        ln_amnt = input()
+    while invalid_loan(amount):
+        prompt("Please enter a valid amount. (e.g. $250,000 = 250000)")
+        amount = input()
 
+    return float(amount)
+
+def get_int_rate():
     prompt("Please enter an interest rate. (e.g. 6.5% = 6.5)")
     int_rate = input()
 
     while invalid_int(int_rate):
-        prompt("Please enter a valid interest rate.")
+        prompt("Please enter a valid interest rate. (e.g. 6.5% = 6.5)")
         int_rate = input()
 
+    return float(int_rate)
+
+def get_loan_duration():
     prompt("Please enter the loan duration in years.")
-    ln_dur = input()
+    loan_duration = input()
 
-    while invalid_dur(ln_dur):
+    while invalid_dur(loan_duration):
         prompt("Please enter a valid duration.")
-        ln_dur = input()
+        loan_duration = input()
 
-    annual_int = float(int_rate) / 100
-    mnthly_int = annual_int / 12
-    ln_amnt = float(ln_amnt)
-    mnths_dur = int(ln_dur) * 12
-    mnthly_p = round(ln_amnt * (mnthly_int / (1 - (1 + mnthly_int)
-                ** (-mnths_dur))), 2)
+    return float(loan_duration)
 
-    prompt(f'''With a loan amount of ${ln_amnt}, an interest rate of '''
-    f'''{int_rate}%, and a duration of {ln_dur} years, your monthly '''
-    f'''payment is: ${mnthly_p}''')
+def compute_monthly_payment(loan, rate, duration):
+    if rate != 0:
+        return round(loan * (((rate / 100) / 12) / (1 - (1 + ((rate / 100) / 12))
+                    ** (-duration * 12))), 2)
+    else:
+        return round(loan / (duration * 12), 2)
 
+def display_result(amount, rate, duration, payment):
+    prompt(f'''With a loan amount of ${amount}, an interest rate of '''
+    f'''{rate}%, and a duration of {duration} years, your monthly '''
+    f'''payment is: ${payment}''')
+
+def perform_new_calc():
     prompt("Would you like to calculate another loan? y/n")
     try_again = input()
-
-    while try_again not in ['y', 'n']:
+    while try_again not in ['y', 'n', 'Y', 'N']:
         prompt("Please enter a valid answer (y or n)")
         try_again = input()
+    return try_again.lower()
 
-    if try_again != 'y':
+while True:
+    loan_amount = get_loan_amount()
+    int_rate = get_int_rate()
+    loan_duration = get_loan_duration()
+    monthly_payment = compute_monthly_payment(
+                    loan_amount, int_rate, loan_duration
+                    )
+    display_result(loan_amount, int_rate, loan_duration, monthly_payment)
+
+
+    answer = perform_new_calc()
+
+    if answer == 'y':
+        os.system('clear')
+    else:
         break

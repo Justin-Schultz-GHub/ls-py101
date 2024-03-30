c30999ddf703d492fe5239d20e5f2c0307d1ef9b
Justin Schultz <SchultzJustin@ymail.com>
2024-03-30 02:52:13 +0000

    Renamed some variables and parameters

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -49,25 +49,25 @@ def player_choice():
 def computer_choice():
     return random.choice(VALID_CHOICES)
 
-def determine_win(player_choice, computer_choice):
-    prompt(f'''You chose {player_choice}, the computer chose '''
-    f'''{computer_choice}.''')
+def determine_win(player, computer):
+    prompt(f'''You chose {player}, the computer chose '''
+    f'''{computer}.''')
 
-    match player_choice, computer_choice:
+    match player, computer:
         case "Rock", "Scissors":
-            return f'{player_choice} beats {computer_choice}. The player wins!'
+            return f'{player} beats {computer}. The player wins!'
         case "Scissors", "Paper":
-            return f'{player_choice} beats {computer_choice}. The player wins!'
+            return f'{player} beats {computer}. The player wins!'
         case "Paper", "Rock":
-            return f'{player_choice} beats {computer_choice}. The player wins!'
+            return f'{player} beats {computer}. The player wins!'
         case "Rock", "Paper":
-            return f'{computer_choice} beats {player_choice}. The computer wins!'
+            return f'{computer} beats {player}. The computer wins!'
         case "Paper", "Scissors":
-            return f'{computer_choice} beats {player_choice}. The computer wins!'
+            return f'{computer} beats {player}. The computer wins!'
         case "Scissors", "Rock":
-            return f'{computer_choice} beats {player_choice}. The computer wins!'
+            return f'{computer} beats {player}. The computer wins!'
         case _:
-            return f'Both players chose {player_choice}. It\'s a tie!'
+            return f'Both players chose {player}. It\'s a tie!'
 
 def play_again():
     prompt("Play again? y/n")
@@ -78,9 +78,9 @@ def play_again():
     return play.lower()
 
 while True:
-    choice1 = player_choice()
-    choice2 = computer_choice()
-    winner = determine_win(choice1, choice2)
+    player_choice = player_choice()
+    computer_choice = computer_choice()
+    winner = determine_win(player_choice, computer_choice)
     prompt(winner)
 
     answer = play_again()

904d054eda00838362ce1c0a2a829649011a5bae
Justin Schultz <SchultzJustin@ymail.com>
2024-04-04 22:12:35 +0000

    Fixed typo in score_keeper(), and removed all nested functions from start_game().

--- lesson_2/rps_bonus_features.py
+++ lesson_2/rps_bonus_features.py
@@ -44,83 +44,83 @@ def player_wins(player, computer):
 def computer_wins(player, computer):
     return player in WINNING_COMBOS[computer]
 
-def start_game():
-    clear_screen()
-    def player_pick():
-        prompt("""Player, enter the corresponding number to make your """
-                """choice (i.e. Rock = 1): \n"""
-                """1. Rock\n"""
-                """2. Paper\n"""
-                """3. Scissors\n"""
-                """4. Lizard\n"""
-                """5. Spock"""
-                )
+def player_pick():
+    prompt("""Player, enter the corresponding number to make your """
+            """choice (i.e. Rock = 1): \n"""
+            """1. Rock\n"""
+            """2. Paper\n"""
+            """3. Scissors\n"""
+            """4. Lizard\n"""
+            """5. Spock"""
+            )
+    choice = input()
+
+    while choice not in ["1", "2", "3", "4", "5"]:
+        prompt("""Please enter a valid choice """
+            """(i.e. Rock = 1): \n"""
+            """1. Rock\n"""
+            """2. Paper\n"""
+            """3. Scissors\n"""
+            """4. Lizard\n"""
+            """5. Spock"""
+            )
         choice = input()
 
-        while choice not in ["1", "2", "3", "4", "5"]:
-            prompt("""Please enter a valid choice """
-                """(i.e. Rock = 1): \n"""
-                """1. Rock\n"""
-                """2. Paper\n"""
-                """3. Scissors\n"""
-                """4. Lizard\n"""
-                """5. Spock"""
-                )
-            choice = input()
+    choice = match_input_to_choice(choice)
+    return choice
 
-        choice = match_input_to_choice(choice)
-        return choice
+def match_input_to_choice(player_input):
+    match player_input:
+        case "1":
+            return VALID_CHOICES[0]
+        case "2":
+            return VALID_CHOICES[1]
+        case "3":
+            return VALID_CHOICES[2]
+        case "4":
+            return VALID_CHOICES[3]
+        case "5":
+            return VALID_CHOICES[4]
 
-    def match_input_to_choice(player_input):
-        match player_input:
-            case "1":
-                return VALID_CHOICES[0]
-            case "2":
-                return VALID_CHOICES[1]
-            case "3":
-                return VALID_CHOICES[2]
-            case "4":
-                return VALID_CHOICES[3]
-            case "5":
-                return VALID_CHOICES[4]
+def computer_pick():
+    return random.choice(VALID_CHOICES)
 
-    def computer_pick():
-        return random.choice(VALID_CHOICES)
+def round_announce():
+    if game_values["game_round"] != 0:
+        print('')
 
-    def score_keeper(winner, player, computer):
-        if winner:
-            prompt(f'{player} beats {computer}! Player +1!')
-            game_values["player_score"] += 1
-        elif not winner:
-            if not tie_checker(player_wins(player_choice, computer_choice),
-                computer_wins(player_choice, computer_choice)):
-                prompt(f'{computer} beats {player}! Computer +1!')
-                game_values["computer_score"] += 1
-            else:
-                prompt(f'Both players chose {player}.')
+    game_values["game_round"] += 1
+    if game_values["game_round"] == 1:
+        return prompt(f'Round {game_values["game_round"]} - Best of 5')
+    return prompt(f'Round {game_values["game_round"]}')
 
-    def score_announce(score1, score2):
-        if score1 < SCORE_LIMIT and score2 < SCORE_LIMIT:
-            return f'The score is {score1} to {score2}.'
+def score_announce(score1, score2):
+    if score1 < SCORE_LIMIT and score2 < SCORE_LIMIT:
+        return f'The score is {score1} to {score2}.'
 
-        if score1 > score2:
-            return f'The player wins! Final score: {score1} to {score2}.'
+    if score1 > score2:
+        return f'The player wins! Final score: {score1} to {score2}.'
 
-        return f'The computer wins! Final score: {score1} to {score2}.'
+    return f'The computer wins! Final score: {score1} to {score2}.'
 
-    def round_announce():
-        if game_values["game_round"] != 0:
-            print('')
+def pick_announce(player, computer):
+    prompt(f'''You chose {player}, the computer chose '''
+    f'''{computer}.''')
 
-        game_values["game_round"] += 1
-        if game_values["game_round"] == 1:
-            return prompt(f'Round {game_values["game_round"]} - Best of 5')
-        return prompt(f'Round {game_values["game_round"]}')
+def score_keeper(winner, player, computer):
+    if winner:
+        prompt(f'{player} beats {computer}! Player +1!')
+        game_values["player_score"] += 1
+    elif not winner:
+        if not tie_checker(player_wins(player, computer),
+            computer_wins(player, computer)):
+            prompt(f'{computer} beats {player}! Computer +1!')
+            game_values["computer_score"] += 1
+        else:
+            prompt(f'Both players chose {player}.')
 
-    def pick_announce(player, computer):
-        prompt(f'''You chose {player}, the computer chose '''
-        f'''{computer}.''')
-
+def start_game():
+    clear_screen()
     while True:
         round_announce()
         player_choice = player_pick()
